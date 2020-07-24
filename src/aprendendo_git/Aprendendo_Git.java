9293ac684fb1568b3fe3a8690ef45ae407e72dbb
Andre Luiz <andre.gomesjr@live.comgit config --listgit config --listgit config --global user.email andre.gomesjr@live.com exiteeeclearexitgit>
2020-07-23 23:46:42 -0300

    Acrescentado mensagem de bom dia

--- src/aprendendo_git/Aprendendo_Git.java
+++ src/aprendendo_git/Aprendendo_Git.java
@@ -15,7 +15,7 @@ public class Aprendendo_Git {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       System.out.println("Ola Mundo");
     }
     
 }
